17b565feca8a4635d935f0a5ac3a93185875f446
Mateusz <chryscienko24141@stud.akademiabialska.pl>
2025-10-29 20:27:16 +0100

    Wersja ostateczna 2

--- Form1.cs
+++ Form1.cs
@@ -29,15 +29,21 @@ namespace lab03
 
                 txtTotal.Text = Result.ToString();
                 txtPure.Text = ResultPure.ToString();
-
-                if (Capacity <= 0 || Count <= 0 || Percent < 0 || Percent>100)
+                
+                if (Capacity <= 0 || Count <= 0)
                 {
-                    txtTotal.Clear();
-                    txtPure.Clear();
-                    MessageBox.Show("Wartoœci musz¹ byæ dodatnie, a procent nie mo¿e przekraczaæ 100!", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show("Pojemnoœæ i liczba musz¹ byæ dodatnie!",
+                                    "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
-
                 }
+
+                if (Percent < 0 || Percent > 100)
+                {
+                    MessageBox.Show("Zawartoœæ substancji musi byæ w zakresie 0–100%!",
+                                    "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
 
             }
             catch
